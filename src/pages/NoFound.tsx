2a138b5f9e43708ae1e12eeafbb4d2bd3489d480
Gassan <gquintar@gmail.com>
2023-09-12 12:49:48 +0200

    feat: page 404

--- src/pages/NoFound.tsx
+++ src/pages/NoFound.tsx
@@ -1,7 +1,19 @@
+import { Box, Card, Typography } from '@mui/material';
 import React, { FC } from 'react';
 
 const NoFound: FC = () => {
-  return <div>404 - Page Not Found</div>;
+  return (
+    <>
+      <Box className='flex justify-center flex-col justify-items-center gap-3 m-[25%]'>
+        <Typography variant='h3' gutterBottom>
+          404 - Page Not Found
+        </Typography>
+        <Typography variant='body1' className='text-center'>
+          Unfortunately, this page can't be found.
+        </Typography>
+      </Box>
+    </>
+  );
 };
 
 export default NoFound;
